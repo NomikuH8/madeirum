ee1376c14541e7fdad69a0ed7c40f31296508d4e
NomikuH8 <sofaco.checkin@gmail.com>
2022-05-22 14:54:11 -0300

    making login functions

--- server.py
+++ server.py
@@ -1,13 +1,46 @@
 from dotenv import dotenv_values
+
+from flask import render_template
+from flask import make_response
+from flask import send_file
 from flask import request
 from flask import Flask
 import psycopg2
+import random
+import utils
 import sys
+import os
 
 PORT = 5000
 
 app = Flask(__name__)
 config = dotenv_values('.env')
+
+tmp_path = ''
+for i in config['BUILD_PATH'].split('/'):
+    tmp_path = os.path.join(tmp_path, i)
+BUILD_PATH = tmp_path
+del tmp_path
+
+create_table = {
+    'usuarios': '''
+    CREATE TABLE
+        IF NOT EXISTS usuarios (
+            id_usuario BIGSERIAL PRIMARY KEY,
+            nome VARCHAR(60) NOT NULL,
+            email VARCHAR(60) NOT NULL,
+            senha VARCHAR(60) NOT NULL
+        )
+    ''',
+    'usuarios_auth': '''
+    CREATE TABLE
+        IF NOT EXISTS usuarios_auth (
+            id_token BIGSERIAL PRIMARY KEY,
+            id_usuario INTEGER NOT NULL,
+            auth_token VARCHAR NOT NULL UNIQUE
+        )
+    '''
+}
 
 def get_connection():
     try:
@@ -30,6 +63,12 @@ else:
     sys.exit(1)
 
 curr = conn.cursor()
+
+for i in create_table.keys():
+    curr.execute(create_table[i])
+
+conn.commit()
+
 
 @app.route('/api/get_usuario')
 def get_usuario():
@@ -57,7 +96,6 @@ def get_usuario():
 
     return ret
 
-
 @app.route('/api/cadastrar_usuario')
 def cadastrar_usuario():
     args = request.args
@@ -83,7 +121,55 @@ def cadastrar_usuario():
         conn.commit()
         return { 'success': True }
 
+@app.route('/api/logar_usuario', methods=['POST'])
+def logar_usuario():
+    args = request.args 
+    try:
+        email = args['email']
+        senha = args['senha']
+    except:
+        return { 'help': 'missing arguments', 'success': False }
+    
+    curr.execute('SELECT id, senha FROM usuarios WHERE email=%s', [email])
+    data = []
+    try:
+        data = curr.fetchall()[0]
+    except:
+        return { 'help': 'user not found', 'success': False }
+    
+    if data[1] != senha:
+        return { 'help': 'wrong password', 'success': False }
+    
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789'
+    randLength = 30
+    decryptedToken = ''.join(random.choice(alphabet) for i in range(randLength))
+    encryptedToken = utils.encryptText(decryptedToken)
 
+    resp = make_response(os.path.join(BUILD_PATH, 'index.html'))
+    resp.set_cookie('sessionToken', encryptedToken)
+
+    curr.execute('INSERT INTO usuarios_auth (id_usuario, auth_token) VALUES (%s, %s)', [data[0], encryptedToken])
+    conn.commit()
+    return resp
+
+@app.route('/api/deslogar_usuario')
+def deslogar_usuario():
+    args = request.args
+    try:
+        email = args['email']
+    except:
+        return { 'help': 'missing arguments', 'success': False }
+    
+    # TODO: finish auth
+
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    if os.path.exists(path):
+        return send_file(path)
+    return send_file(os.path.join(BUILD_PATH, "index.html"))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT, debug=True, threaded=True)
