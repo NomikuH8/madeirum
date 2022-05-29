d7cc68cae6969ecf66fa55648658b3916fe50f37
NomikuH8 <sofaco.checkin@gmail.com>
2022-05-29 15:09:43 -0300

    started title screen

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -1,12 +1,34 @@
-import { Link } from "react-router-dom";
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { checkLogin, getFile } from "./utility/utils";
+import { useNavigate } from 'react-router';
+import { useCookies } from "react-cookie";
+import Div from "./styles/rootStyle";
+import { useEffect } from "react";
 
 function Root() {
+  const [cookies] = useCookies(['access-token'])
+  const navi = useNavigate()
+
+  useEffect(() => {
+    const token = cookies['access-token']
+    checkLogin(token, navi, null)
+  }, [])
+
+  const handleClick = (to: string) => {
+    navi(to)
+  }
+
   return (
-    <div>
-      <Link to={'/login'}>Login</Link>
-      <br />
-      <Link to={'/cadastro'}>Cadastro</Link>
-    </div>
+    <Div>
+      <div id="whiteCover">
+        <img src={getFile('favicon.ico')} alt="" />
+        <div>
+          <button className="loginBtn" onClick={() => handleClick('/login')}>Fazer login</button>
+          <button className="cadastroBtn" onClick={() => handleClick('/cadastrar')}>Criar conta</button>
+        </div>
+      </div>
+    </Div>
   )
 }
 
