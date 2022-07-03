3b250ab317f52150b1fdbfcc76ee42ba6ce2f329
NomikuH8 <sofaco.checkin@gmail.com>
2022-07-03 18:05:43 -0300

    making pedidos page

--- client/src/pages/Pedidos.tsx
+++ client/src/pages/Pedidos.tsx
@@ -1,7 +1,44 @@
+import { Receipt } from "@mui/icons-material"
+import { useEffect, useState } from "react"
+import PedidosDiv from "../styles/pedidosStyle"
+import { getOrders } from "../utility/orderWrapper"
+
 function Pedidos() {
+  const [orders, setOrders]: any = useState([])
+
+  useEffect(() => {
+    getOrders(setOrders)
+  }, [])
+
+  let conteudo = <></>
+
+  if (orders === [])
+    conteudo = <div>Carregando...</div>
+  
+  if (typeof orders === 'object')
+    conteudo = <div className="nenhum-pedido">Nenhum pedido até agora :(</div>
+
+  if (orders.length > 0) {
+    conteudo = orders.map((pedido: any, idx: number) => {
+      return (
+        <div className="pedido">
+          <span>Pedido #{pedido[0]}</span>
+        </div>
+      )
+    })
+  }
+  
   return (
-    <div>
-    </div>
+    <PedidosDiv>
+      <div className="title">
+        <Receipt />
+        <span>Pedidos</span>
+        <Receipt />
+      </div>
+      <div className="pedidos">
+        {conteudo}
+      </div>
+    </PedidosDiv>
   )
 }
 
---
+++ client/src/styles/pedidosStyle.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+const PedidosDiv = styled.div`
+`
+
+export default PedidosDiv
