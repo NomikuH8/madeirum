5fef171b742e8133e78f139f528232914b526bdd
NomikuH8 <sofaco.checkin@gmail.com>
2022-07-02 21:44:02 -0300

    now orders stack up if they are equal

--- client/src/utility/orderWrapper.ts
+++ client/src/utility/orderWrapper.ts
@@ -4,13 +4,22 @@ export {
   sendOrder
 }
 
-function addOrder(data: object) {
-  let cart: Array<object> | null = JSON.parse(localStorage.getItem('cart')!)
+function addOrder(data: any) {
+  let cart: Array<any> | null = JSON.parse(localStorage.getItem('cart')!)
   if (cart === null) {
     localStorage.setItem('cart', JSON.stringify([data]))
     return
   }
 
+  for (let i = 0; i < cart.length; i++) {
+    if (data.nome_produto === cart[i].nome_produto) {
+      if (data.observation === cart[i].observation) {
+        cart[i].quantity++
+        localStorage.setItem('cart', JSON.stringify(cart))
+        return
+      }
+    }
+  }
   cart.push(data)
   localStorage.setItem('cart', JSON.stringify(cart))
 }
